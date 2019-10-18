e89c97379c81bab733ea4c4ee3b28374c2c0d8aa
Pacôme <pacome.lehoux@laposte.net>
2019-10-18 12:44:27 +0200

    added basic javafx hello world app

--- src/main/java/fr/insa/colisvif/App.java
+++ src/main/java/fr/insa/colisvif/App.java
@@ -3,12 +3,38 @@
  */
 package fr.insa.colisvif;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello World.";
     }
 
+    @Override
+    public void start(Stage stage) throws IOException {
+        // load main scene
+        Parent root = FXMLLoader.load(getClass().getResource("/scene.fxml"));
+        Scene scene = new Scene(root);
+
+        // apply css
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+
+        // init and show stage
+        stage.setTitle("Hello World");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        // launch javafx app
+        launch(args);
     }
 }
