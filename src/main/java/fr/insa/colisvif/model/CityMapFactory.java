a5e2883ea985edb757fb982d88360790755f62cd
Pacôme <pacome.lehoux@laposte.net>
2019-10-22 10:19:23 +0200

    fixed CityMapFactory.createCityMapFromXMLFile not expecting a File as input

--- src/main/java/fr/insa/colisvif/model/CityMapFactory.java
+++ src/main/java/fr/insa/colisvif/model/CityMapFactory.java
@@ -7,48 +7,28 @@ import fr.insa.colisvif.xml.CityMapParserXML;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 public class CityMapFactory {
-    private static CityMapFactory instance;
-    private CityMap cityMap;
     private CityMapParser cityMapParserXML;
 
-    private CityMapFactory() {
-        this.cityMap = new CityMap();
+    public CityMapFactory() {
         this.cityMapParserXML = new CityMapParserXML();
     }
 
-    //TODO on doit pouvoir passer le fichier en paramètre !
-    public CityMap createCityMapFromXMLFile() throws IOException, SAXException, ParserConfigurationException {
-        this.cityMapParserXML.loadFile();
+    public CityMap createCityMapFromXMLFile(File file) throws IOException, SAXException, ParserConfigurationException {
+        this.cityMapParserXML.loadFile(file);
+        CityMap cityMap = new CityMap();
         List<Triplet<Long, Double, Double>> nodes = this.cityMapParserXML.readNodes();
         List<Quadruplet<Double, String, Long, Long>> sections = this.cityMapParserXML.readSections();
         for (Triplet<Long, Double, Double> node : nodes) {
-            this.makeNode(node.getFirst(), node.getSecond(), node.getThird());
+            cityMap.createNode(node.getFirst(), node.getSecond(), node.getThird());
         }
         for(Quadruplet<Double, String, Long, Long> section : sections) {
-            this.makeSection(section.getFirst(), section.getSecond(), section.getThird(), section.getFourth());
+            cityMap.createSection(section.getFirst(), section.getSecond(), section.getThird(), section.getFourth());
         }
-        return this.cityMap;
-    }
-
-    public void makeNode(long id, double latitude, double longitude) {
-        this.cityMap.createNode(id, latitude, longitude);
-    }
-
-    public void makeSection(double length, String roadName, long destination, long origin) {
-        this.cityMap.createSection(length, roadName, destination, origin);
-    }
-
-    public CityMap getCityMap() {
-        return this.cityMap;
-    }
-
-    public static CityMapFactory getInstance() {
-        if (instance == null)
-            instance = new CityMapFactory();
-        return instance;
+        return cityMap;
     }
 }
