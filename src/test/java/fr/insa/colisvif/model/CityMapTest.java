6051ed33f5b9e5b7d68b7646b6485b45b35e6aa4
Guilhem Cerba <guilhem.cerba@insa-lyon.fr>
2019-10-22 11:08:35 +0200

    Update CityMapTest.java
    Tests citymap latmax longmin

--- src/test/java/fr/insa/colisvif/model/CityMapTest.java
+++ src/test/java/fr/insa/colisvif/model/CityMapTest.java
@@ -4,4 +4,30 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CityMapTest {
+
+    @Test
+    public void testUpdateLatMax() {
+        CityMap citymap = new CityMap();
+        citymap.createNode(2405632, -60, 24);
+
+        int expected = -60;
+        assert(expected == citymap.getLatMax());
+    }
+
+    @Test
+    public void testUpdateLongMin() {
+        CityMap citymap = new CityMap();
+        citymap.createNode(2405632, -90, 120);
+
+        int expected = 120;
+        assert(expected == citymap.getLongMin());
+    }
+
+    @Test
+    public void testUpdateLatMaxLongMin() {
+        CityMap citymap = new CityMap();
+        citymap.createNode(2405632, -60, 120);
+        assert(-60 == citymap.getLatMax() && citymap.getLongMin() == 120) ;
+    }
+
 }
