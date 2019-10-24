376c56ec90927f38abfc197b14b77f9d47e12ea8
Pacôme <pacome.lehoux@laposte.net>
2019-10-24 14:20:49 +0200

    modified jfx controller to interract with mvc controller

--- src/main/java/fr/insa/colisvif/view/MainController.java
+++ src/main/java/fr/insa/colisvif/view/MainController.java
@@ -1,7 +1,7 @@
 package fr.insa.colisvif.view;
 
+import fr.insa.colisvif.controller.Controller;
 import fr.insa.colisvif.model.CityMap;
-import fr.insa.colisvif.model.CityMapFactory;
 import fr.insa.colisvif.model.Node;
 import fr.insa.colisvif.model.Section;
 import javafx.event.ActionEvent;
@@ -11,11 +11,8 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,8 +38,11 @@ public class MainController {
 
     private Stage stage;
 
-    public MainController(Stage stage) {
+    private Controller controller;
+
+    public MainController(Stage stage, Controller controller) {
         this.stage = stage;
+        this.controller = controller;
     }
 
     public void initialize() {
@@ -51,27 +51,23 @@ public class MainController {
 
         this.openMap.addEventHandler(ActionEvent.ACTION, event -> {
             File file = fileChooser.showOpenDialog(this.stage);
-            this.openFile(file);
+            this.controller.openFile(file);
         });
 
         this.close.addEventHandler(ActionEvent.ACTION, event -> stage.close());
     }
 
-    public void openFile(File file) {
-        CityMapFactory factory = new CityMapFactory();
-        try {
-            CityMap map = factory.createCityMapFromXMLFile(file);
-            this.drawMap(map);
-        }
-        catch(IOException | SAXException | ParserConfigurationException e) {
-            e.printStackTrace();
-        }
+    public void clearMap() {
+        var context = this.canvas.getGraphicsContext2D();
+        context.clearRect(0, 0,
+                          this.canvas.getWidth(),
+                          this.canvas.getHeight());
     }
 
     public void drawMap(CityMap map) {
-        var context = canvas.getGraphicsContext2D();
-        final int latToPx = 10000;
-        final int lngToPx = 10000;
+        var context = this.canvas.getGraphicsContext2D();
+        final int LAT_TO_PX = 10000;
+        final int LNG_TO_PX = 10000;
 
         final Color NODE_COLOR = Color.CORNFLOWERBLUE;
         final Color SECTION_COLOR = Color.BLACK;
@@ -80,28 +76,37 @@ public class MainController {
         Map<Long, Node> nodes = map.getMapNode();
 
         context.setFill(SECTION_COLOR);
-        List<Section> sections = map.getMapSection().values().stream().reduce(new ArrayList<>(), (acc, val) -> {
-            acc.addAll(val);
-            return acc;
-        });
-        for(Section section : sections) {
+        List<Section> sections = map
+            .getMapSection()
+            .values()
+            .stream()
+            .reduce(new ArrayList<>(), (acc, val) -> {
+                acc.addAll(val);
+                return acc;
+            });
+        for (Section section : sections) {
             Node origin = nodes.get(section.getOrigin());
             Node destination = nodes.get(section.getDestination());
 
-            double x1 = (origin.getLongitude() - map.getLongMin()) * lngToPx;
-            double y1 = (map.getLatMax() - origin.getLatitude()) * latToPx;
+            double x1 = (origin.getLongitude() - map.getLongMin()) * LNG_TO_PX;
+            double y1 = (map.getLatMax() - origin.getLatitude()) * LAT_TO_PX;
 
-            double x2 = (destination.getLongitude() - map.getLongMin()) * lngToPx;
-            double y2 = (map.getLatMax() - destination.getLatitude()) * latToPx;
+            double x2 = (destination.getLongitude() - map.getLongMin())
+                        * LNG_TO_PX;
+            double y2 = (map.getLatMax() - destination.getLatitude())
+                        * LAT_TO_PX;
 
             context.strokeLine(x1, y1, x2, y2);
         }
 
         context.setFill(NODE_COLOR);
-        for(Node node : nodes.values()) {
-            double x = (node.getLongitude() - map.getLongMin()) * lngToPx;
-            double y = (map.getLatMax() - node.getLatitude()) * latToPx;
-            context.fillOval(x - NODE_SIZE / 2d, y - NODE_SIZE / 2d, NODE_SIZE, NODE_SIZE);
+        for (Node node : nodes.values()) {
+            double x = (node.getLongitude() - map.getLongMin()) * LNG_TO_PX;
+            double y = (map.getLatMax() - node.getLatitude()) * LAT_TO_PX;
+            context.fillOval(x - NODE_SIZE / 2d,
+                             y - NODE_SIZE / 2d,
+                             NODE_SIZE,
+                             NODE_SIZE);
         }
     }
 }
