b74dd05526b4ba7765e5ae841b2124476b1265ae
BorsukUlamThm <49310803+BorsukUlamThm@users.noreply.github.com>
2019-10-25 19:25:04 +0200

    Ajout de la tournée et des étapes

---
+++ src/main/java/fr/insa/colisvif/model/Step.java
@@ -0,0 +1,24 @@
+package fr.insa.colisvif.model;
+
+import java.util.List;
+
+public class Step {
+    private List<Section> sections;
+    private boolean type; //0 if it is a pick up and 1 if it is a drop off
+    private int arrivalDateInSeconds; //the date when the delivery man will get to delivery point
+    private int durationInSeconds; //the duration of the pick up or the drop off, NOT THE TRAVEL TIME
+
+    public List<Section> getSections() { return sections; }
+
+    public int getArrivalDateInSeconds() { return arrivalDateInSeconds; }
+
+    public void setArrivalDateInSeconds(int arrivalDateInSeconds) { this.arrivalDateInSeconds = arrivalDateInSeconds; }
+
+    public int getDurationInSeconds() { return durationInSeconds; }
+
+    public void setDurationInSeconds(int durationInSeconds) { this.durationInSeconds = durationInSeconds; }
+
+    public boolean isPickUp() { return type; }
+
+    public boolean isDropOff() { return !type; }
+}
