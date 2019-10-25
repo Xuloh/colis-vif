27d80b9769554798355c1b32e32d2482e53c69f5
Guilhem Cerba <guilhem.cerba@insa-lyon.fr>
2019-10-25 10:30:06 +0200

    Debut

---
+++ src/test/java/fr/insa/colisvif/model/DeliveryTest.java
@@ -0,0 +1,11 @@
+package fr.insa.colisvif.model;
+
+import org.junit.Test;
+
+public class DeliveryTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testOriginEqualsDestination() {
+        //Delivery delivery = new Node(100, 100, 10,10);
+    }
+}
