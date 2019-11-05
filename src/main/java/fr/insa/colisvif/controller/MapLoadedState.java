a3f854e8b8c40e06103719eef075942e5abc5bb3
bvachez <brice.vachez@insa-lyon.fr>
2019-11-05 08:53:39 +0100

    Squelettes des états actuels

---
+++ src/main/java/fr/insa/colisvif/controller/MapLoadedState.java
@@ -0,0 +1,16 @@
+package fr.insa.colisvif.controller;
+
+import fr.insa.colisvif.view.MainController;
+
+public class MapLoadedState implements State{
+
+    @Override
+    public void loadCityMap(Controller c, MainController mc) {
+
+    }
+
+    @Override
+    public void loadDeliveryMap(Controller c, MainController mc) {
+
+    }
+}
--- src/main/java/fr/insa/colisvif/controller/State.java
+++ src/main/java/fr/insa/colisvif/controller/State.java
@@ -1,4 +1,10 @@
 package fr.insa.colisvif.controller;
 
+import fr.insa.colisvif.view.MainController;
+
 public interface State {
+
+    public default void loadCityMap(Controller c, MainController mc) {};
+
+    public default void loadDeliveryMap(Controller c, MainController mc) {};
 }
