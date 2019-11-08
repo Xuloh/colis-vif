f1772208b9ad91c33f017979c4b50f6d0bc16acd
mathieu <matmont98@gmail.com>
2019-11-08 09:16:11 +0100

    add section test

--- src/test/java/fr/insa/colisvif/model/SectionTest.java
+++ src/test/java/fr/insa/colisvif/model/SectionTest.java
@@ -6,18 +6,18 @@ import static org.junit.Assert.*;
 public class SectionTest {
 
     @Test
-    public void testSectionGood () {
-        Section section = new Section (20,"Rue de la Paix",100,101);
+    public void testSectionGood() {
+        Section section = new Section(20, "Rue de la Paix", 100, 101);
     }
 
-    @Test (expected = IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testSectionNegativeLength() {
-        Section section = new Section(-20,"Rue de la Paix",100,101);
+        Section section = new Section(-20, "Rue de la Paix", 100, 101);
     }
 
-    @Test (expected = IllegalArgumentException.class)
-    public void testSectionZeroLength () {
-        Section section = new Section (0,"Rue de la Paix",100,101);
+    @Test(expected = IllegalArgumentException.class)
+    public void testSectionZeroLength() {
+        Section section = new Section(0, "Rue de la Paix", 100, 101);
     }
 
     /*
@@ -28,43 +28,43 @@ public class SectionTest {
     */
 
     @Test
-    public void testSectionToString () {
-        Section section = new Section (20,"Rue de la Paix",100,101);
-        assertEquals ("Length : 20.0 | Road Name : Rue de la Paix | Destination : 100 | Origin : 101\n",section.toString());
+    public void testSectionToString() {
+        Section section = new Section(20, "Rue de la Paix", 100, 101);
+        assertEquals("Length : 20.0 | Road Name : Rue de la Paix | Destination : 100 | Origin : 101\n", section.toString());
     }
 
 
     @Test
-    public void getLength () {
+    public void getLength() {
         long expected = 20;
-        Section section = new Section(expected,"Rue de la Paix",100,101);
-        assertEquals (expected, section.getLength (), 0.001);
+        Section section = new Section(expected, "Rue de la Paix", 100, 101);
+        assertEquals(expected, section.getLength(), 0.001);
     }
 
     @Test
-    public void getRoadName () {
+    public void getRoadName() {
         String expected = "Rue de la Paix";
-        Section section = new Section(20, expected,100,101);
-        assertEquals (expected, section.getRoadName ());
+        Section section = new Section(20, expected, 100, 101);
+        assertEquals(expected, section.getRoadName());
     }
 
     @Test
-    public void getDestination () {
+    public void getDestination() {
         long expected = 100;
-        Section section = new Section (20,"Rue de la Paix",expected,101);
-        assertEquals (expected, section.getDestination ());
+        Section section = new Section(20, "Rue de la Paix", expected, 101);
+        assertEquals(expected, section.getDestination());
     }
 
     @Test
-    public void testEquals () {
-        Section section = new Section (20,"Rue de la Paix",100,101);
-        Section expected = new Section (20,"Rue de la Paix",100,101);
-        assertEquals (expected, section);
-        assertEquals (section, section);
+    public void testEquals() {
+        Section section = new Section(20, "Rue de la Paix", 100, 101);
+        Section expected = new Section(20, "Rue de la Paix", 100, 101);
+        assertEquals(expected, section);
+        assertEquals(section, section);
 
-        expected = new Section (20,"Rue de la Paix",101,101);
-        assertNotEquals (expected, section);
-        expected = new Section (20,"Rue de la Paix",100,100);
-        assertNotEquals (expected, section);
+        expected = new Section(20, "Rue de la Paix", 101, 101);
+        assertNotEquals(expected, section);
+        expected = new Section(20, "Rue de la Paix", 100, 100);
+        assertNotEquals(expected, section);
     }
 }
