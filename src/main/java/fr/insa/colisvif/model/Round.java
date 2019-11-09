66ea20a4bee3017d6603948c0c3032b40d0d41f7
mathieu <matmont98@gmail.com>
2019-11-09 17:21:44 +0100

    Added squeletton for modify, add and delete delivery

--- src/main/java/fr/insa/colisvif/model/Round.java
+++ src/main/java/fr/insa/colisvif/model/Round.java
@@ -30,6 +30,99 @@ public class Round {
         startDate = deliveries.getStartDateInSeconds();
     }
 
+    /**
+     * Remove the delivery composed by 2 steps from the list of steps
+     * @param stepPickup the pickup from the delivery
+     * @param stepDelivery the dropoff from the delivery
+     */
+    public void removeDelivery(Step stepPickup, Step stepDelivery){             // Make callable with Delivery or one step
+        if (!(steps.contains(stepPickup) && steps.contains(stepDelivery))) {    // The steps are in the list steps
+            throw new IllegalArgumentException();
+        }
+        if (stepPickup.getDeliveryID() != stepDelivery.getDeliveryID()) {       // The steps correspond to the same delivery
+            throw new IllegalArgumentException();
+        }
+
+        // Change the list section of the next step
+        Step stepAfterPickup = steps.get(steps.indexOf(stepPickup) + 1);
+        Step stepAfterDelivery = null;
+
+        if(steps.indexOf(stepDelivery) != steps.size() - 1){
+            stepAfterDelivery = steps.get(steps.indexOf(stepDelivery) + 1);
+        }
+
+        // Remove the steps
+        steps.remove(stepPickup);
+        steps.remove(stepDelivery);
+
+        // Change all hours
+        for (int i = steps.indexOf(stepAfterPickup); i < steps.size(); i ++){
+
+        }
+    }
+
+    /**
+     * Add a new delivery to the round. Add two steps, one for the pickup and one for the delivery
+      * @param stepPickup step to go from the previous location to the pickup location
+     * @param stepDelivery step to go from the previous location to the delivery pickup
+     */
+    public void addDelivery(Step stepPickup, Step stepDelivery){
+        // /!\ calculer le chemin !!!!!!!!!!
+        //
+        if (stepPickup.getDeliveryID() != stepDelivery.getDeliveryID()) {   // The steps are in the list steps
+            throw new IllegalArgumentException();
+        }
+
+        /*
+            MANQUE LE CALCUL DE TEMPS
+            MODIFIER LES LISTES DES SECTIONS EMPRUNTÉES
+         */
+        steps.add(stepPickup);
+        steps.add(stepDelivery);
+
+        // Change all hours
+        for(int i = steps.indexOf(stepPickup); i < steps.size(); i ++){
+
+        }
+    }
+
+    /**
+     * Change the order of stepChangeOrder to be just before stepJustAfter
+     * @param stepChangeOrder the step that the order will be changed
+     * @param stepJustAfter the step that will follow stepChangeOrder
+     */
+    public void changeOrderStep(Step stepChangeOrder, Step stepJustAfter){
+        if (!(steps.contains(stepChangeOrder) && steps.contains(stepJustAfter))) { // The steps are in the list steps
+            throw new IllegalArgumentException();
+        }
+
+        int start = 0;
+        int end = 0;
+
+        if (stepChangeOrder.isPickUp()) {           // stepChangeOrder is a pickup, check that the delivery is not before
+            end = steps.indexOf(stepJustAfter);
+        } else {                                    // stepChangeOrder is a delivery, check that the pickup is not after
+            start = steps.indexOf(stepJustAfter);
+            end = steps.size();
+        }
+
+        for (int i = start; i < end; i++) {
+            if (steps.get(i).getDeliveryID() == stepChangeOrder.getDeliveryID()) {
+               throw new IllegalArgumentException();    // One of the previous assessment is false
+            }
+        }
+
+        steps.remove(stepChangeOrder);
+        steps.add(steps.indexOf(stepJustAfter), stepChangeOrder);
+
+        // /!\ CHANGE THE LIST OF STEPS FOR stepJustAfter
+
+        // Change all hours
+        for(int i = steps.indexOf(stepChangeOrder); i < steps.size(); i ++){
+
+        }
+    }
+
     public void pushStep(Step step){
         steps.add(step);
     }
--- src/main/java/fr/insa/colisvif/model/Step.java
+++ src/main/java/fr/insa/colisvif/model/Step.java
@@ -1,6 +1,7 @@
 package fr.insa.colisvif.model;
 
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class Step {
     private LinkedList<Section> sections;
@@ -13,6 +14,9 @@ public class Step {
     public int getArrivalDate() { return arrivalDate; }
     public void setArrivalDate(int arrivalDate) { this.arrivalDate = arrivalDate; }
     public int getDuration() { return duration; }
+    public int getDeliveryID() {
+        return deliveryID;
+    }
     public void setDuration(int durationInSeconds) { this.duration = durationInSeconds; }
     public boolean isPickUp() { return type; }
     public boolean isDropOff() { return !type; }
@@ -26,5 +30,21 @@ public class Step {
 
     public void pushSection(Section section){
         sections.addFirst(section);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Step step = (Step) o;
+        return deliveryID == step.deliveryID
+                && type == step.type
+                && arrivalDate == step.arrivalDate
+                && duration == step.duration
+                && Objects.equals(sections, step.sections);
     }
 }
