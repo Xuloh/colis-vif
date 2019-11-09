52403f85dfaf1b6f0c592fb60a940d3ce0a550bf
Pacôme <pacome.lehoux@laposte.net>
2019-11-09 15:07:32 +0100

    added class to hold constants for the canvas

---
+++ src/main/java/fr/insa/colisvif/view/CanvasConstants.java
@@ -0,0 +1,20 @@
+package fr.insa.colisvif.view;
+
+import javafx.scene.paint.Color;
+
+public final class CanvasConstants {
+
+    public static final double MIN_ZOOM_SCALE = 1d;
+
+    public static final double MAX_ZOOM_SCALE = 5d;
+
+    public static final double DELTA_ZOOM_SCALE = 0.1d;
+
+    public static final Color SECTION_COLOR = Color.grayRgb(85);
+
+    public static final int DELIVERY_NODE_SIZE = 15;
+
+    private CanvasConstants() {
+
+    }
+}
