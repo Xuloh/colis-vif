f4b5127e392491a46ad2038018569e068aab2a2a
SoPlump <sophie.raudrant@gmail.com>
2019-11-10 16:22:10 +0100

    Deleting of SuppressedNodeSelectedState

--- src/main/java/fr/insa/colisvif/controller/state/PickUpNodeAddedState.java
+++ src/main/java/fr/insa/colisvif/controller/state/PickUpNodeAddedState.java
@@ -2,6 +2,14 @@ package fr.insa.colisvif.controller.state;
 
 import fr.insa.colisvif.controller.Controller;
 
+/**
+ * Class that implements State interface.
+ * This class represents the state when, in add mode, the pick up node is already chosen.
+ * It overrides all the actions that can be done during this state.
+ *
+ * @see State
+ * @see <a href="https://en.wikipedia.org/wiki/State_pattern">State pattern</a>
+ */
 public class DropOffNodeAdding implements State {
 
     @Override
