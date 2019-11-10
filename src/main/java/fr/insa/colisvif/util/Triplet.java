e7f50d80c8ca183ac305f87aac9bd3bea534b5b9
Pacôme <pacome.lehoux@laposte.net>
2019-11-10 12:14:50 +0100

    added Javadoc to Triplet

--- src/main/java/fr/insa/colisvif/util/Triplet.java
+++ src/main/java/fr/insa/colisvif/util/Triplet.java
@@ -5,29 +5,80 @@ package fr.insa.colisvif.util;
 
 import java.util.Objects;
 
+/**
+ * A convenience class to hold together 3 values
+ * @param <T> the type of 1st value
+ * @param <U> the type of 2nd value
+ * @param <V> the type of 3rd value
+ */
 public class Triplet<T, U, V> {
 
     private final T first;
+
     private final U second;
+
     private final V third;
 
+    /**
+     * Creates a new {@link Triplet} with the given values
+     * @param first the 1st value
+     * @param second the 2nd value
+     * @param third the 3rd value
+     */
     public Triplet(T first, U second, V third) {
         this.first = first;
         this.second = second;
         this.third = third;
     }
 
+    /**
+     * Determines if the given {@link Object} is "equal"
+     * to this {@link Triplet}.
+     * Only other {@link Triplet} are considered for comparison.
+     * The method compares the 3 values of this {@link Triplet}
+     * with those of the other {@link Triplet} by calling their respective
+     * <code>equals</code> method.
+     * @param o the {@link Object} to compare this {@link Triplet} to
+     * @return <code>true</code> if o is a {@link Triplet} whose values are
+     * "equal" to those of this {@link Triplet}
+     */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
         Triplet<?, ?, ?> triplet = (Triplet<?, ?, ?>) o;
-        return Objects.equals(first, triplet.first) &&
-                Objects.equals(second, triplet.second) &&
-                Objects.equals(third, triplet.third);
+        return Objects.equals(first, triplet.first)
+               && Objects.equals(second, triplet.second)
+               && Objects.equals(third, triplet.third);
     }
 
-    public T getFirst() { return first; }
-    public U getSecond() { return second; }
-    public V getThird() { return third; }
+    /**
+     * Returns the 1st value of this {@link Triplet}
+     * @return the 1st value of this {@link Triplet}
+     */
+    public T getFirst() {
+        return first;
+    }
+
+    /**
+     * Returns the 2nd value of this {@link Triplet}
+     * @return the 2nd value of this {@link Triplet}
+     */
+    public U getSecond() {
+        return second;
+    }
+
+    /**
+     * Returns the 3rd value of this {@link Triplet}
+     * @return the 3rd value of this {@link Triplet}
+     */
+    public V getThird() {
+        return third;
+    }
 }
