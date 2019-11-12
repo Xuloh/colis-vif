5f5a516e42b9e40a38e6fb7f15e837ec6cff35a0
bvachez <brice.vachez@insa-lyon.fr>
2019-11-12 16:03:13 +0100

    Added new Commands that will work with Felix's new Round methods

---
+++ src/main/java/fr/insa/colisvif/controller/command/CommandAdd.java
@@ -0,0 +1,29 @@
+package fr.insa.colisvif.controller.command;
+
+import fr.insa.colisvif.model.*;
+
+public class CommandAdd implements Command{
+
+    private Vertex pickUpVertex;
+
+    private Vertex dropOffVertex;
+
+    private Round round;
+
+    public CommandAdd(Vertex pickUpVertex, Vertex dropOffVertex, Round round) {
+        this.pickUpVertex = pickUpVertex;
+        this.dropOffVertex = dropOffVertex;
+        this.round = round;
+    }
+
+    @Override
+    public void undoCommand() {
+        /*round.removeDelivery(signatureDeLaNouvelleMethode) #MethodeFelix*/
+    }
+
+    @Override
+    public void doCommand() {
+        /*round.addDelivery(pickUpVertex.getNodeId(), dropOffVertex.getNodeId(),
+                          pickUpVertex.getDuration(), dropOffVertex.getDuration()); #MethodeFelix*/
+    }
+}
