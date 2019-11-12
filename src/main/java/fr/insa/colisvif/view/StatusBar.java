fe97b6a7df09bf5bc7028f947219be3dcfb02121
SoPlump <sophie.raudrant@gmail.com>
2019-11-12 19:10:07 +0100

    Adding bug logger items

--- src/main/java/fr/insa/colisvif/view/StatusBar.java
+++ src/main/java/fr/insa/colisvif/view/StatusBar.java
@@ -4,9 +4,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public class StatusBar extends HBox {
+
+    private static final Logger LOGGER = LogManager.getLogger(StatusBar.class);
 
     private Label message;
 
@@ -28,10 +32,14 @@ public class StatusBar extends HBox {
     public void setStatus(String text) {
         this.message.setTextFill(Color.rgb(0, 0, 0));
         this.message.setText(text);
+        StackTraceElement caller = Thread.currentThread().getStackTrace()[3];
+        LOGGER.debug("Status view message : {}\nCaller : {}.{}", text, caller.getClassName(), caller.getMethodName());
     }
 
     public void setError(String text) {
         this.message.setTextFill(Color.rgb(255, 0, 0));
         this.message.setText(text);
+        StackTraceElement caller = Thread.currentThread().getStackTrace()[3];
+        LOGGER.debug("Status view error : {}\nCaller : {}.{}", text, caller.getClassName(), caller.getMethodName());
     }
 }
