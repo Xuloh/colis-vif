239d869bb4e5c6350cb39046e60cc3394c63eb25
bvachez <brice.vachez@insa-lyon.fr>
2019-11-13 09:50:50 +0100

    Commands implemented + States linked to

--- src/main/java/fr/insa/colisvif/controller/state/ModifyStopLocationState.java
+++ src/main/java/fr/insa/colisvif/controller/state/ModifyStopLocationState.java
@@ -1,6 +1,10 @@
 package fr.insa.colisvif.controller.state;
 
 import fr.insa.colisvif.controller.Controller;
+import fr.insa.colisvif.controller.command.CommandList;
+import fr.insa.colisvif.controller.command.CommandModifyLocation;
+import fr.insa.colisvif.model.Step;
+import fr.insa.colisvif.view.UIController;
 
 /**
  * Class that implements State interface.
@@ -12,12 +16,18 @@ import fr.insa.colisvif.controller.Controller;
  */
 public class ModifyStopLocationState implements State {
 
+    private Step stepToChange;
+
     /**
      * When in location change mode, allow the user to edit the node location.
      */
     @Override
-    public void changeNodeLocation() {
-
+    public void leftClick(Controller controller, UIController uiController, CommandList commandList) {
+        Long nodeIdSelected = null; //todo : récup avec la méthode @Pacome
+        if (nodeIdSelected != null) {
+            long nodeId = nodeIdSelected;
+            commandList.doCommand(new CommandModifyLocation(stepToChange, nodeId, controller.getRound(), controller.getCityMap()));
+        }
     }
 
     /**
@@ -29,5 +39,9 @@ public class ModifyStopLocationState implements State {
     @Override
     public void getBackToPreviousState(Controller controller) {
         controller.setCurrentState(PropertiesPrintedState.class);
+    }
+
+    protected void entryToState(Step stepToModify) {
+        this.stepToChange = stepToModify;
     }
 }
