16e71d30cb11b954cdec1ed311c6d75903020c75
mathieu <matmont98@gmail.com>
2019-11-14 23:57:37 +0100

    Node 80 characters

--- src/main/java/fr/insa/colisvif/model/Node.java
+++ src/main/java/fr/insa/colisvif/model/Node.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A class representing a Node, formed with an id, a longitude and latitude in degrees, and a {@link List} of successors ({@link Section}).
+ * A class representing a Node, formed with an id, a longitude and latitude in
+ * degrees, and a {@link List} of successors ({@link Section}).
  */
 public class Node {
 
@@ -24,19 +25,23 @@ public class Node {
      * @param id the id of the Node.
      * @param latitude the longitude of the Node.
      * @param longitude the longitude of the Node.
-     * @throws IllegalArgumentException if the latitude is not between -90 and 90 and/or the longitude is not between -180 and 180.
+     * @throws IllegalArgumentException if the latitude is not between -90 and
+     * 90 and/or the longitude is not between -180 and 180.
      */
-    public Node(long id, double latitude, double longitude) throws IllegalArgumentException {
+    public Node(long id, double latitude, double longitude)
+            throws IllegalArgumentException {
         this.id = id;
         this.longitude = longitude;
         this.latitude = latitude;
         this.successors = new ArrayList<>();
 
         if (this.latitude < -90 || this.latitude > 90) {
-            throw new IllegalArgumentException("The latitude must be between -90 and 90, got " + latitude);
+            throw new IllegalArgumentException("The latitude must be between "
+                    + "-90 and 90, got " + latitude);
         }
         if (this.longitude > 180 || this.longitude < -180) {
-            throw new IllegalArgumentException("The longitude must be between -180 and 180, got " + longitude);
+            throw new IllegalArgumentException("The longitude must be between "
+                    + "-180 and 180, got " + longitude);
         }
     }
 
@@ -77,26 +82,32 @@ public class Node {
     }
 
     /**
-     * Add a {@link Section} to the {@link List} of successors of the current {@link Node}.
+     * Add a {@link Section} to the {@link List} of successors of the
+     * current {@link Node}.
      * @param section the {@link Section} to add.
-     * @throws IdException if the origin of the new {@link Section} is different from the id of the current {@link Node}.
+     * @throws IdException if the origin of the new {@link Section} is
+     * different from the id of the current {@link Node}.
      */
     public void addToSuccessors(Section section) throws IdException {
         if (section.getOrigin() == this.id) {
             this.successors.add(section);
         } else {
-            throw new IdException("The origin of the new section does not correspond to the origin of this Node. Got " + section.getOrigin() + " instead of " + this.id);
+            throw new IdException("The origin of the new section does not "
+                    + "correspond to the origin of this Node. Got "
+                    + section.getOrigin() + " instead of " + this.id);
         }
     }
 
     /**
      * Sets the longitude of the current {@link Node}.
      * @param longitude the longitude to set.
-     * @throws IllegalArgumentException if the longitude is less or more than 180
+     * @throws IllegalArgumentException if the longitude is less or more than
+     * 180
      */
     public void setLongitude(double longitude) {
         if (longitude > 180 || longitude < -180) {
-            throw new IllegalArgumentException("The longitude must be between -180 and 180, got " + longitude);
+            throw new IllegalArgumentException("The longitude must be between "
+                    + "-180 and 180, got " + longitude);
         }
         this.longitude = longitude;
     }
@@ -109,7 +120,8 @@ public class Node {
      */
     public void setLatitude(double latitude) {
         if (latitude < -90 || latitude > 90) {
-            throw new IllegalArgumentException("The latitude must be between -90 and 90, got " + latitude);
+            throw new IllegalArgumentException("The latitude must be between "
+                    + "-90 and 90, got " + latitude);
         }
         this.latitude = latitude;
     }
@@ -120,7 +132,8 @@ public class Node {
      */
     @Override
     public String toString() {
-        String result = "ID : " + id + " | Latitude : " + latitude + " | Longitude : " + longitude + "\n";
+        String result = "ID : " + id + " | Latitude : " + latitude
+                + " | Longitude : " + longitude + "\n";
         return result;
     }
 
