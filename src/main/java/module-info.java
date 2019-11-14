a233e96711ede10504975e80ab4effefa4984109
Pacôme <pacome.lehoux@laposte.net>
2019-11-14 19:54:47 +0100

    added missing opens statement in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,5 +5,11 @@ module colisvif.main {
     requires javafx.fxml;
     requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
+    opens fr.insa.colisvif.controller;
+    opens fr.insa.colisvif.controller.command;
+    opens fr.insa.colisvif.controller.state;
+    opens fr.insa.colisvif.exception;
+    opens fr.insa.colisvif.model;
+    opens fr.insa.colisvif.util;
     opens fr.insa.colisvif.view;
 }
