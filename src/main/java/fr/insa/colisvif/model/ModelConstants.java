04fb4b448a581d08d46f3b4ca0d45c601a310072
mathieu <matmont98@gmail.com>
2019-11-14 23:56:27 +0100

    ModelConstants 80 characters

--- src/main/java/fr/insa/colisvif/model/ModelConstants.java
+++ src/main/java/fr/insa/colisvif/model/ModelConstants.java
@@ -9,7 +9,8 @@ public final class ModelConstants {
     /**
      * Speed of the cyclist
      */
-    public static final int CYCLIST_SPEED = (int) (15. / 3.6); //15 km.h-1 (the speed is in m.s-1)
+    public static final int CYCLIST_SPEED = (int) (15. / 3.6);
+    //15 km.h-1 (the speed is in m.s-1)
 
     public static final int INTERVAL_LENGTH = 2 * 60 * 60; //2 hours
 
